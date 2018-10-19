f7b84cfafbed5af568cddf15f800b0661d09b30e
Aliaksei Hrechushkin <d.cells.aloha@gmail.com>
2018-10-19 16:21:13 +0300

    image

--- Program.cs
+++ Program.cs
@@ -13,22 +13,32 @@ namespace Awesome {
     static List <string> Strigalev = new List<string>();
     static List <string> Protchenko = new List<string>();
     static List <string> Memas = new List<string>();
-    
+
+
     static void Main() {
       botClient = new TelegramBotClient("676101534:AAGQMTb_4eHW_VstJDnK60ps8hx6jsP9OD4");
       Butin.Add("Для тех, кто не говорит по-русски... *шёпотом* А здесь такие есть");
       Butin.Add("Так это, как это ваша фамилия?");
       Butin.Add("Ясно, понятно.");
       Butin.Add("Достаточно");
+      Butin.Add("Один белый убил четырёх красных.");
+      
       Strigalev.Add("Семантические связи, молодые люди.");
       Strigalev.Add("Я вам в тысячный раз повторяю, эпам вас обманывает.");
       Strigalev.Add("Есть правила игры и по ним нужно играть");
+      
       Protchenko.Add("Я пришла сюда работать не ради денег.");
       Protchenko.Add("Ну чё, сдаём или по домам?");
       Protchenko.Add("Я вам покажу, как развернуть Докер-контейнер, когда-нибудь");
       Protchenko.Add("Пойдёте лабы Гончаревичу сдвать");
+      
       Memas.Add("https://raw.githubusercontent.com/donwhispers/chatbotssp/master/mems/izGUdkA7itU.jpg");
       Memas.Add("https://raw.githubusercontent.com/donwhispers/chatbotssp/master/mems/K0a0QHM67nk.jpg");
+      Memas.Add("https://raw.githubusercontent.com/donwhispers/chatbotssp/master/mems/cq0D4htFVOo.jpg");
+      Memas.Add("https://raw.githubusercontent.com/donwhispers/chatbotssp/master/mems/dpESvZRLMIE.jpg");
+      Memas.Add("https://raw.githubusercontent.com/donwhispers/chatbotssp/master/mems/fKEjfI5ajbE.jpg");
+      Memas.Add("https://raw.githubusercontent.com/donwhispers/chatbotssp/master/mems/bOb-Ji8DALk.jpg");
+      
       var me = botClient.GetMeAsync().Result;
       Console.WriteLine(
         $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
